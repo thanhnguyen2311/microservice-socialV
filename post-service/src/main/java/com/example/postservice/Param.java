e871184e5739e6fdae1089f8c5f93545d0f48f0e
Thanh Nguyen <ntthanhdb1999@gmail.com>
2025-02-04 22:45:41 +0700

    deploy bằng dockerfile và xây dựng docker compose

--- post-service/src/main/java/com/example/postservice/Param.java
+++ post-service/src/main/java/com/example/postservice/Param.java
@@ -1,7 +1,7 @@
 package com.example.postservice;
 
 public class Param {
-    public static final String baseUserUrl = "http://localhost:8080";
+    public static final String baseUserUrl = "http://host.docker.internal:8080";
 
     public static final String FUNCTION_CHECK_USER_EXIST = "/user/check-exist";
     public static final String FUNCTION_CHECK_FRIENDSHIP = "/friend/check-friend";
